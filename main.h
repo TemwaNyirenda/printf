9b13a4815e00e3a06ae6840a645ec6907e21b344
GerhardP27 <gerhardpillay.gp@gmail.com>
2022-07-10 13:16:59 -0700

    task 1,2

--- main.h
+++ main.h
@@ -2,9 +2,20 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+/**
+ * itoa - converts integer to a string
+ */
+char *itoa(long int, int);
 void print_perc(va_list __attribute__ ((unused)) vargs, int *ptr2count);
 void print_char(va_list vargs, int *ptr2count);
 void print_str(va_list vargs, int *ptr2count);
