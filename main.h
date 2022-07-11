1df232df9d20fe7a88e225447d7cca9e5d4575b6
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-11 12:34:48 -0700

    Task #5, created print_capitalS.c and edited format_spec_match.c and main.h

--- main.h
+++ main.h
@@ -8,16 +8,20 @@ int _printf(const char *format, ...);
 int format_spec_match(char c, va_list vargs, int *ptr2count, char *buffer,
 		int *buf_index);
 
+/* Task #0 */
 void print_perc(va_list __attribute__ ((unused)) vargs, int *ptr2count,
 		char *buffer, int *buf_index);
 void print_char(va_list vargs, int *ptr2count, char *buffer, int *buf_index);
 void print_str(va_list vargs, int *ptr2count, char *buffer, int *buf_index);
 
+/* Task #1 */
 void print_int(va_list vargs, int *ptr2count, char *buffer, int *buf_index);
 char *itoa(long int num, int base);
 
+/* Task #2 */
 void print_binary(va_list vargs, int *ptr2count, char *buffer, int *buf_index);
 
+/*Task #3 */
 void print_unsigned(va_list vargs, int *ptr2count, char *buffer,
 		int *buf_index);
 void print_octal(va_list args, int *ptr2count, char *buffer, int *buf_index);
@@ -25,7 +29,12 @@ void print_hexadecimal_low(va_list vargs, int *ptr2count, char *buffer,
 		int *buf_index);
 void print_hexadecimal_upp(va_list vargs, int *ptr2count, char *buffer,
 		int *buf_index);
+int is_lowercase(char c);
+char *string_to_upper(char *s);
 
+/* Task #5 */
+void print_capitalS(va_list vargs, int *ptr2count, char *buffer,
+                int *buf_index);
 
 /**
  * format_list - a struct with matching format specifiers with their functions
