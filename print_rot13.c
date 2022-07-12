ffb95f15cf5068c0b80cea25223e28aba54baabc
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-12 12:51:21 -0700

    Task #14, correction #1

--- print_rot13.c
+++ print_rot13.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * print_rot13 - prints a rot13'd string
@@ -15,6 +16,11 @@ void print_rot13(va_list vargs, int *ptr2count, char *buffer, int *buf_index)
 {
 	int i;
 	char *str = va_arg(vargs, char *);
+	char *rot13_str;
+
+	rot13_str = malloc(sizeof(char) * _strlen(str));
+	if (rot13_str == NULL)
+		return;
 
 	if (str == NULL)
 	{
@@ -27,20 +33,24 @@ void print_rot13(va_list vargs, int *ptr2count, char *buffer, int *buf_index)
 			if ((str[i] >= 65 && str[i] <= 77) ||
 				(str[i] >= 97 && str[i] <= 109))
 			{
-				str[i] = str[i] + 13;
+				rot13_str[i] = str[i] + 13;
 			}
 			else if ((str[i] >= 78 && str[i] <= 90) ||
 				(str[i] >= 110 && str[i] <= 122))
 			{
-				str[i] = str[i] - 13;
+				rot13_str[i] = str[i] - 13;
 			}
+			else
+				rot13_str[i] = str[i];
 		}
 	}
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; rot13_str[i] != '\0'; i++)
 	{
-		buffer[*buf_index] = str[i];
+		buffer[*buf_index] = rot13_str[i];
 		*buf_index += 1;
 		*ptr2count += 1;
 	}
+
+	free(rot13_str);
 }
